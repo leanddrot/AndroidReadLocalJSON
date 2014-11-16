ea1f9d2671d6f05d68b5a3f8ae3d75f302a14ddb
Leandro Torroija <leanddro@hotmail.com>
2014-11-16 17:27:19 -0300

    Refactorizacion de la SettingsActivity

--- app/src/main/java/fourheads/org/readlocaljson/SettingsActivity.java
+++ app/src/main/java/fourheads/org/readlocaljson/SettingsActivity.java
@@ -23,51 +23,28 @@ import java.io.InputStreamReader;
 
 public class SettingsActivity extends Activity {
 
-    EditText editTextUrl;
-    Button buttonSave;
-    JSONObject obj = null;
-    String urlRestful;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        editTextUrl = (EditText)findViewById(R.id.editText_settings_url);
-        buttonSave = (Button)findViewById(R.id.button_guardar);
+        final EditText editTextUrl = (EditText)findViewById(R.id.editText_settings_url);
+        Button buttonSave = (Button)findViewById(R.id.button_guardar);
+        final Activity activity = this;
 
-        String json = loadJSON();
+        final GestionConfigRepositorio gestionConfigRepositorio = new GestionConfigRepositorio();
+        final GestionConfig config = gestionConfigRepositorio.recuperarConfiguracion(activity);
 
-        try {
-            obj = new JSONObject(json);
-            urlRestful = obj.getString("urlRestful");
-            editTextUrl.setText(urlRestful);
-
-        } catch (JSONException e) {
-
-            e.printStackTrace();
-        }
-
-
+        editTextUrl.setText(config.getUrlRestful());
 
         buttonSave.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                try {
-                    obj.put("urlRestful", editTextUrl.getText());
-                    FileOutputStream fos = openFileOutput("config.json", Context.MODE_PRIVATE);
-                    fos.write(obj.toString().getBytes());
-                    fos.close();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
 
+                final GestionConfig config = gestionConfigRepositorio.recuperarConfiguracion(activity);
+                config.setUrlRestful(editTextUrl.getText().toString());
+                gestionConfigRepositorio.guardarConfiguracion(activity, config);
 
                 finish();
             }
@@ -75,50 +52,4 @@ public class SettingsActivity extends Activity {
 
     }
 
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
-    public String loadJSON() {
-        String json = null;
-        try {
-            FileInputStream fis = openFileInput("config.json");
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader bufferedReader = new BufferedReader(isr);
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                sb.append(line);
-            }
-
-            json = sb.toString();
-
-        } catch (FileNotFoundException e) {
-            json = "{ \"urlRestful\" : \" \",  \"user\" : \"\",  \"pass\" : \"\",  \"save\" : \"\"}";
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return json;
-
-    }
 }
